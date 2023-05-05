5ea131ab4671629bb63962da8da17d28cb2575bc
pappbbence <papp.bence321@gmail.com>
2023-05-05 11:43:32 +0200

    Initial commit

---
+++ src/test/java/hu/nye/progkor/clothes/ClothesApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.nye.progkor.clothes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
